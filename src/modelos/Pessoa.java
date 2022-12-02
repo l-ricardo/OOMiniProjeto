ff2e1fc654e6d7c11b5c460907b777624b6dfe27
Ricardo <lrsj2003@gmail.com>
2022-12-02 19:51:26 -0300

    cria pessoa

---
+++ src/modelos/Pessoa.java
@@ -0,0 +1,32 @@
+package modelos;
+
+public abstract class Pessoa {
+    String nome;
+    char sexo;
+    int idade;
+
+    // Gets e Sets
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
