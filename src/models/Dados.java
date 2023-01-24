385dc50a8c94ecf1700304f12480f8c192393b75
Ricardo <lrsj2003@gmail.com>
2023-01-24 05:37:30 -0300

    dados iniciais funcionando

--- src/models/Dados.java
+++ src/models/Dados.java
@@ -1,41 +1,19 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Dados {
     private ArrayList<Canal> canais = new ArrayList<>();
-    // private ArrayList<Horario> horarios = new ArrayList<>();
     private ArrayList<Artista> artistas = new ArrayList<>();
     private ArrayList<Personagem> personagens = new ArrayList<>();
     private ArrayList<Pessoa> pessoas = new ArrayList<>();
     private ArrayList<NovelaFilmeSerie> programaTipoNovelaFilmeSerie = new ArrayList<>();
-    private ArrayList<NovelaFilmeSerie> programaTipoTalkShow = new ArrayList<>();
+    private ArrayList<TalkShow> programaTipoTalkShow = new ArrayList<>();
     private ArrayList<Jornal> programaTipoJornal = new ArrayList<>();
 
     // Criando valores de exemplo
     {
-        // for (int i = 0; i < 24; i++) {
-        // horarios.add(new Horario(i, i + 1, "Domingo"));
-        // }
-        // for (int i = 24; i < 48; i++) {
-        // horarios.add(new Horario(i - 24, i - 24 + 1, "Segunda"));
-        // }
-        // for (int i = 48; i < 72; i++) {
-        // horarios.add(new Horario(i - 48, i - 48 + 1, "Terça"));
-        // }
-        // for (int i = 72; i < 96; i++) {
-        // horarios.add(new Horario(i - 72, i - 72 + 1, "Quarta"));
-        // }
-        // for (int i = 96; i < 120; i++) {
-        // horarios.add(new Horario(i - 96, i - 96 + 1, "Quinta"));
-        // }
-        // for (int i = 120; i < 144; i++) {
-        // horarios.add(new Horario(i - 120, i - 120 + 1, "Sexta"));
-        // }
-        // for (int i = 144; i < 168; i++) {
-        // horarios.add(new Horario(i - 144, i - 144 + 1, "Sabado"));
-        // }
-
         artistas.add(new Artista("Ana Maria Braga", 'F', "Apresentadora"));
         artistas.add(new Artista("Louro Mané", 'M', "Humorista"));
         artistas.add(new Artista("Rodrigo Faro", 'M', "Apresentador"));
@@ -45,111 +23,80 @@ public class Dados {
         artistas.add(new Artista("Renata Vasconcelos", 'F', "Jornalista"));
         artistas.add(new Artista("Lucio Mauro", 'M', "Musico", "Vocal e guitarra"));
 
-        personagens.add(new Personagem("Carminha", 'F', new Pessoa("Adriana Esteves", 'F'), true));
-        personagens.add(new Personagem("Tufão", 'M', new Pessoa("Murilo Benicio", 'M')));
-        personagens.add(new Personagem("Nina/Rita", 'F', new Pessoa("Debora Falabella", 'F'), true));
-        personagens.add(new Personagem("11", 'F', new Pessoa("Millie Bobby Brown", 'F'), true));
-        personagens.add(new Personagem("Mike", 'M', new Pessoa("Finn Wolfhard", 'M'), true));
-        personagens.add(new Personagem("Will Byers", 'M', new Pessoa("Noah Schnapp", 'M'), true));
-        personagens.add(new Personagem("Lucas Sinclair", 'M', new Pessoa("Caleb McLaughlin", 'M'), true));
-        personagens.add(new Personagem("Dustin Henderson", 'M', new Pessoa("Gaten Matarazzo", 'M'), true));
-        personagens.add(new Personagem("Bob Newby", 'M', new Pessoa("Sean Astin", 'M'), false));
+        personagens.add(new Personagem("Carminha", 'F',
+                new Pessoa("Adriana Esteves", 'F'), true));
+        personagens.add(new Personagem("Tufão", 'M',
+                new Pessoa("Murilo Benicio", 'M')));
+        personagens.add(new Personagem("Nina/Rita", 'F',
+                new Pessoa("Debora Falabella", 'F'), true));
+        personagens.add(new Personagem("11", 'F',
+                new Pessoa("Millie Bobby Brown", 'F'), true));
+        personagens.add(new Personagem("Mike", 'M',
+                new Pessoa("Finn Wolfhard", 'M'), true));
+        personagens.add(new Personagem("Will Byers", 'M',
+                new Pessoa("Noah Schnapp", 'M'), true));
+        personagens.add(new Personagem("Lucas Sinclair", 'M',
+                new Pessoa("Caleb McLaughlin", 'M'), true));
+        personagens.add(new Personagem("Dustin Henderson", 'M',
+                new Pessoa("Gaten Matarazzo", 'M'), true));
+        personagens.add(new Personagem("Bob Newby", 'M',
+                new Pessoa("Sean Astin", 'M'), false));
+        personagens.add(new Personagem("Bob Esponja", 'M',
+                new Pessoa("Wendell Bezerra", 'M'), true));
 
         pessoas.add(new Pessoa("Dona Maria", 'F'));
         pessoas.add(new Pessoa("Seu João", 'M'));
 
-        // TODO: Corrigir constrututores de programas
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Avenida Brasil",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(7), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 1, 300));
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Cobras e Lagartos",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(7), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 1, 300));
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Vingadores",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(7), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 1, 300));
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("A Favorita",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(7), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 1, 300));
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Bagulhos Sinistros",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(7), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 4, 32));
-        // programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Bob Esponja",
-        // new ArrayList<Horario>(Arrays.asList(horarios.get(54), horarios.get(14),
-        // horarios.get(31), horarios.get(50))),
-        // 16, new ArrayList<Personagem>(Arrays.asList(personagens.get(0),
-        // personagens.get(1),
-        // personagens.get(2))),
-        // 15, 800));
+        programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Avenida Brasil",
+                new ArrayList<Integer>(Arrays.asList(2, 3, 4, 5, 6)), "21:00", 120, 16,
+                new ArrayList<Personagem>(Arrays.asList(personagens.get(0), personagens.get(1), personagens.get(2))), 1,
+                300));
+        programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Bagulhos Sinistros",
+                new ArrayList<Integer>(Arrays.asList(7)), "7:00", 60, 12,
+                new ArrayList<Personagem>(Arrays.asList(personagens.get(3), personagens.get(4), personagens.get(5),
+                        personagens.get(6), personagens.get(7), personagens.get(8))),
+                4, 32));
+        programaTipoNovelaFilmeSerie.get(1).setFavorito(true);
+        programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Vingadores",
+                new ArrayList<Integer>(Arrays.asList(6)), "13:00", 200, 14,
+                new ArrayList<Personagem>(Arrays.asList(personagens.get(0), personagens.get(3))), 0, 1));
+        programaTipoNovelaFilmeSerie.add(new NovelaFilmeSerie("Bob Esponja",
+                new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7)), "10:00", 20, 0,
+                new ArrayList<Personagem>(Arrays.asList(personagens.get(9))), 15, 1200));
+        programaTipoNovelaFilmeSerie.get(3).setAnimado(true);
 
-        // programaTipoTalkShow.add(new TalkShow("Mais Você",new
-        // ArrayList<Horario>(Arrays.asList(horarios.get(2), horarios.get(10),
-        // horarios.get(40), horarios.get(50))),0));
+        programaTipoJornal.add(new Jornal("Jornal Nacional",
+                new ArrayList<Integer>(Arrays.asList(2, 3, 4, 5, 6)), "20:00", 60, 0,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(5), artistas.get(6))), "Nacional"));
 
-        // programaTipoTalkShow.add(new TalkShow("Hora do Faro",new
-        // ArrayList<Horario>(Arrays.asList(horarios.get(25), horarios.get(44),
-        // horarios.get(26), horarios.get(47))),12, new ArrayList<Artista>(Arrays.asList
-        // (artistas.get(2)))));
+        programaTipoTalkShow.add(new TalkShow("Mais Você",
+                new ArrayList<Integer>(Arrays.asList(2, 3, 4, 5, 6)), "8:00", 90, 0,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(0), artistas.get(1)))));
+        programaTipoTalkShow.add(new TalkShow("Hora do Faro",
+                new ArrayList<Integer>(Arrays.asList(7)), "15:00", 60, 10,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(2)))));
+        programaTipoTalkShow.get(1).setConvidados(new ArrayList<Pessoa>(Arrays.asList(pessoas.get(0), pessoas.get(1))));
+        programaTipoTalkShow.add(new TalkShow("Programa do Silvio Santos",
+                new ArrayList<Integer>(Arrays.asList(1)), "16:00", 300, 0,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(3)))));
+        programaTipoTalkShow.add(new TalkShow("Roda Roda Jequiti",
+                new ArrayList<Integer>(Arrays.asList(1)), "5:00", 90, 0,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(3)))));
+        programaTipoTalkShow.add(new TalkShow("Caldeirao",
+                new ArrayList<Integer>(Arrays.asList(7)), "15:00", 240, 0,
+                new ArrayList<Artista>(Arrays.asList(artistas.get(4)))));
+        programaTipoTalkShow.get(4).setBanda(new ArrayList<Artista>(Arrays.asList(artistas.get(7))));
 
-        // programaTipoTalkShow.add(new TalkShow("Programa do Silvio Santos",new
-        // ArrayList<Horario>(Arrays.asList(horarios.get(69), horarios.get(57),
-        // horarios.get(4), horarios.get(7))),12, new ArrayList<Artista>(Arrays.asList
-        // (artistas.get(3)))));
-
-        // programaTipoTalkShow.add(new TalkShow("Roda Roda Jequiti",new
-        // ArrayList<Horario>(Arrays.asList(horarios.get(30), horarios.get(71),
-        // horarios.get(58), horarios.get(75))),12, new ArrayList<Artista>(Arrays.asList
-        // (artistas.get(3)))));
-
-        // programaTipoTalkShow.add(new TalkShow("Caldeirao",new
-        // ArrayList<Horario>(Arrays.asList(horarios.get(78), horarios.get(1),
-        // horarios.get(95), horarios.get(25))),12, new ArrayList<Artista>(Arrays.asList
-        // ( artistas.get(4) )),new ArrayList<Artista>(Arrays.asList ( artistas.get(8)
-        // )), new ArrayList<Pessoa>(Arrays.asList ( pessoas.get(0), pessoas.get(1)
-        // ))));
-
-        // programaTipoJornal.add(new Jornal("Jornal Nacional",
-        // new ArrayList<Horario>(Arrays.asList( horarios.get(0), horarios.get(24),
-        // horarios.get(34), horarios.get(48))), 0,
-        // new ArrayList<Artista>(Arrays.asList (artistas.get(5), artistas.get(6))),
-        // "Nacional"));
-
-        canais.add(new Canal("Globo", 8
-        // ,
-        // programaTipoNovelaFilmeSerie.get(0),
-        // programaTipoJornal.get(0),
-        // programaTipoTalkShow.get(0),
-        // programaTipoTalkShow.get(4)
-        ));
-        canais.add(new Canal("Record", 10
-        // ,
-        // programaTipoTalkShow.get(1)
-        ));
-        canais.add(new Canal("SBT", 2
-        // ,
-        // programaTipoTalkShow.get(2),
-        // programaTipoTalkShow.get(3)
-        ));
+        canais.add(new Canal("Globo", 8,
+                new ArrayList<Programa>(
+                        Arrays.asList(programaTipoNovelaFilmeSerie.get(0), programaTipoNovelaFilmeSerie.get(2),
+                                programaTipoJornal.get(0), programaTipoTalkShow.get(0), programaTipoTalkShow.get(4)))));
+        canais.add(new Canal("Record", 10,
+                new ArrayList<Programa>(
+                        Arrays.asList(programaTipoNovelaFilmeSerie.get(3), programaTipoTalkShow.get(1)))));
+        canais.add(new Canal("SBT", 2,
+                new ArrayList<Programa>(Arrays.asList(programaTipoNovelaFilmeSerie.get(1), programaTipoTalkShow.get(2),
+                        programaTipoTalkShow.get(3)))));
         canais.add(new Canal("a", 91));
         canais.add(new Canal("b", 92));
         canais.add(new Canal("c", 93));
@@ -212,11 +159,11 @@ public class Dados {
         this.programaTipoNovelaFilmeSerie = programaTipoNovelaFilmeSerie;
     }
 
-    public ArrayList<NovelaFilmeSerie> getProgramaTipoTalkShow() {
+    public ArrayList<TalkShow> getProgramaTipoTalkShow() {
         return programaTipoTalkShow;
     }
 
-    public void setProgramaTipoTalkShow(ArrayList<NovelaFilmeSerie> programaTipoTalkShow) {
+    public void setProgramaTipoTalkShow(ArrayList<TalkShow> programaTipoTalkShow) {
         this.programaTipoTalkShow = programaTipoTalkShow;
     }
 
@@ -227,5 +174,4 @@ public class Dados {
     public void setProgramaTipoJornal(ArrayList<Jornal> programaTipoJornal) {
         this.programaTipoJornal = programaTipoJornal;
     }
-
 }
